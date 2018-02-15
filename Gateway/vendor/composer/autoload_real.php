451bb1d3f1e7cb39f21f8942e444dc85e931eeec
Maxime Killinger <max.killinger@outlook.fr>
2018-02-15 10:06:51 +0100

    UPD - SDK Update
    EasyTransac SDK Update in v1.0.11 with composer.
    
    modified:   composer.lock
    modified:   vendor/autoload.php
    modified:   vendor/composer/ClassLoader.php
    modified:   vendor/composer/LICENSE
    modified:   vendor/composer/autoload_real.php
    new file:   vendor/composer/autoload_static.php
    modified:   vendor/composer/installed.json
    modified:   vendor/easytransac/easytransac-sdk-php/sdk/EasyTransac/Core/PaymentNotification.php
    modified:   vendor/easytransac/easytransac-sdk-php/sdk/EasyTransac/Entities/OneClickTransaction.php
    modified:   vendor/easytransac/easytransac-sdk-php/sdk/EasyTransac/Entities/PaymentPageInfos.php
    modified:   vendor/easytransac/easytransac-sdk-php/sdk/EasyTransac/Requests/DebitPayment.php
    modified:   vendor/easytransac/easytransac-sdk-php/sdk/EasyTransac/Requests/DirectPayment.php
    modified:   vendor/easytransac/easytransac-sdk-php/sdk/EasyTransac/Requests/OneClickPayment.php
    modified:   vendor/easytransac/easytransac-sdk-php/sdk/EasyTransac/Requests/PaymentRefund.php
    modified:   vendor/easytransac/easytransac-sdk-php/sdk/EasyTransac/Requests/PaymentStatus.php
    modified:   vendor/easytransac/easytransac-sdk-php/sdk/EasyTransac/Requests/Request.php
    modified:   vendor/easytransac/easytransac-sdk-php/tests/sdk/Entities/PaymentPageInfosTest.php

--- Gateway/vendor/composer/autoload_real.php
+++ Gateway/vendor/composer/autoload_real.php
@@ -23,19 +23,26 @@ class ComposerAutoloaderInitf290913db7217117819bfb71504ad8d2
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitf290913db7217117819bfb71504ad8d2', 'loadClassLoader'));
 
-        $map = require __DIR__ . '/autoload_namespaces.php';
-        foreach ($map as $namespace => $path) {
-            $loader->set($namespace, $path);
-        }
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require_once __DIR__ . '/autoload_static.php';
 
-        $map = require __DIR__ . '/autoload_psr4.php';
-        foreach ($map as $namespace => $path) {
-            $loader->setPsr4($namespace, $path);
-        }
+            call_user_func(\Composer\Autoload\ComposerStaticInitf290913db7217117819bfb71504ad8d2::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
 
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
         }
 
         $loader->register(true);
